6cf903b66e729ddf3aa33ceedd92a089cd2f8b3c
Amar Phule <amardeepphule@gmail.com>
2024-07-15 22:42:56 +0530

    using use client directive param/searchparam

--- src/app/products/[details]/page.tsx
+++ src/app/products/[details]/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+import { useParams, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const ProductDetails = () => {
+  const {details} = useParams()
+  // console.log(details);
+  
+  // http://localhost:3000/products/1?search=amar
+  const search = useSearchParams()
+
+  console.log(search.get('search'));
+  
+
   return (
     <h1 className='text-center text-3xl'>Product Details page</h1>
   )
