be625d753c02479d73b799c7186c3d9fee679b8e
Amar Phule <amardeepphule@gmail.com>
2024-07-15 22:21:49 +0530

    serverside redirect (restric page)

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,10 @@
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Account = () => {
+  const useIsNull = null
+  if(useIsNull == null) redirect("profile")
+   
   return (
     <h1 className='text-center text-3xl'>Account page</h1>
   )
