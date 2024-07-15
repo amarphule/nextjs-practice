d6de3bbb820bc3e2a34c101316de6c24f0474081
Amar Phule <amardeepphule@gmail.com>
2024-07-15 23:02:18 +0530

    get server side param

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from 'next';
+import React from 'react'
+
+type Params = {
+    id: string;
+    searchParams: string;
+  };
+  
+  type UserProps = {
+    params: Params;
+  };
+const User: NextPage<UserProps> = ({params}) => {
+    console.log(params);
+    
+    const {id} = params
+    
+  return (
+    <h1 className='text-center text-3xl'>User page</h1>
+  )
+}
+
+export default User
