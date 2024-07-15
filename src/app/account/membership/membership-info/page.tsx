23f94ee98bfd6b5c1b1bde9a313438c72a3311c7
Amar Phule <amardeepphule@gmail.com>
2024-07-15 21:39:36 +0530

    nested routes

---
+++ src/app/account/membership/membership-info/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MembershipInfo = () => {
+  return (
+    <h1 className='text-center text-3xl'>Membership-info page</h1>
+  )
+}
+
+export default MembershipInfo
---
+++ src/app/account/membership/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Membership = () => {
+  return (
+    <h1 className='text-center text-3xl'>Membership page</h1>
+  )
+}
+
+export default Membership
