0b528c5f712822c715dc17b705dff9ba7a5c6c75
Amar Phule <amardeepphule@gmail.com>
2024-07-15 21:58:30 +0530

    link route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-3">
     <h1>Welcome to Next js Practice</h1>
+    <Link className='border border-slate-800 p-2 rounded-md' href={'products'}>Navigate to Product page</Link>
+    <Link className='border border-slate-800 p-2 rounded-md' href={'account'}>Navigate to Account page</Link>
     </main>
   );
 }
