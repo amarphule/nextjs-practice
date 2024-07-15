4569207cd00db0e3038234854c1697a8c6e79c58
Amar Phule <amardeepphule@gmail.com>
2024-07-15 21:29:48 +0530

    page routes

---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <h1 className='text-center text-3xl'>Account page</h1>
+  )
+}
+
+export default Account
---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <h1 className='text-center text-3xl'>Product page</h1>
+  )
+}
+
+export default Product
